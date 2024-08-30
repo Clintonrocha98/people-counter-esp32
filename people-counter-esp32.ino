2804f85004ab717708fa4f1fb5b13d924745f618
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2024-08-30 00:17:49 -0300

    feat: add http, wifi lib, add function to make post request to the api

--- people-counter-esp32.ino
+++ people-counter-esp32.ino
@@ -1,37 +1,67 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "NOME_DA_REDE";
+const char* password = "SENHA_DA_REDE";
+
+const char* URL = "http://URL-FODA/notifica"
+
 const int ledRedPin = 23; 
 const int ledGreenPin = 12;
 
-const int ECHO_one =27;
-const int TRIG_one =26;
+const int ECHO_one = 27;
+const int TRIG_one = 26;
 
-const int ECHO_two =18;
-const int TRIG_two =17;
-
-const float VelocidadeSom_mpors = 340;
-const float VelocidadeSom_mporus = 0.000340; 
+const int ECHO_two = 18;
+const int TRIG_two = 17;
 
 float TimeEcho_one = 0;
 float TimeEcho_two = 0;
 
-int Pessoas = 0;
+int numberOfPeople = 0;
 
-
-float CalculaDistancia(float tempo_us){
-  return((tempo_us*VelocidadeSom_mporus)/2);
+void ultrasonicPulse(int pin){
+  digitalWrite(pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pin, LOW);
 }
 
-void DisparaPulsoUltrassonico(int TRIG){
-  digitalWrite(TRIG, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG, LOW);
+void ledFlashing(int led){
+    digitalWrite(led,HIGH);
+    delay(1000);
+    digitalWrite(led,LOW);
+}
+
+void requestApi(int number ) {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(URL);
+    http.addHeader("Content-Type", "application/json");
+  
+    int httpResponseCode = http.POST("{numberOfPeople: "+String(number)+"}");
+
+    String response = http.getString();
+    Serial.println(httpResponseCode);
+    Serial.println(response);
+
+    http.end();
+  }
 }
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Hello, ESP32!");
   
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando ao WiFi...");
+  }
+
+  Serial.println("Conectado ao WiFi");
+
   pinMode(TRIG_one, OUTPUT);
   pinMode(ECHO_one, INPUT);
 
@@ -40,46 +70,32 @@ void setup() {
 
   pinMode(ledRedPin, OUTPUT);  
   pinMode(ledGreenPin, OUTPUT);
-  
+
   delay(100);
 }
 
-void piscaLed(int led){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-}
 
 void loop() {
-  
-  DisparaPulsoUltrassonico(TRIG_one);
+  ultrasonicPulse(TRIG_one);
   TimeEcho_one = pulseIn(ECHO_one, HIGH);
 
   delayMicroseconds(50);
 
-  DisparaPulsoUltrassonico(TRIG_two);
+  ultrasonicPulse(TRIG_two);
   TimeEcho_two = pulseIn(ECHO_two, HIGH);
 
-  Serial.print("TimeEcho_one: ");
-  Serial.println(TimeEcho_one);
-  Serial.print("TimeEcho_two: ");
-  Serial.println(TimeEcho_two);
-
-  const float distancia_one = CalculaDistancia(TimeEcho_one);
-  const float distancia_two = CalculaDistancia(TimeEcho_two);
-
   if(TimeEcho_one < TimeEcho_two){
-    Pessoas++;
-    Serial.println("Alguem entrou!");
-    Serial.print("Numero de pessoas na sala: ");
-    Serial.println(Pessoas);
-    piscaLed(ledGreenPin);
+    numberOfPeople++;
+
+    requestApi(numberOfPeople);
+
+    ledFlashing(ledGreenPin);
+  } 
+  if(TimeEcho_one > TimeEcho_two){
+    numberOfPeople--;
     
-  } else {
-    Pessoas--;
-    Serial.println("Alguem saiu!");
-    Serial.print("Numero de pessoas na sala: ");
-    Serial.println(Pessoas);
-    piscaLed(ledRedPin);
+    requestApi(numberOfPeople);
+
+    ledFlashing(ledRedPin);
   }  
 }
